ad700add067e3e793e085a8b65326ae2519b1a69
Nathaniel Navarro <nrn25@cornell.edu>
2022-03-09 17:30:45 -0500

    moving changes on main to this branch (streamer)

--- camlify/src/streamer.ml
+++ camlify/src/streamer.ml
@@ -1,1 +1,31 @@
 (* TODO: Implement according to streamer.mli *)
+<<<<<<< Updated upstream
+=======
+open Gstreamer
+
+type file = int
+
+type streamer = int
+
+let play () =
+  init();
+
+  let pipeline = Pipeline.parse_launch "playbin uri=file://home/navarro/cs3110/camlify/camlify/data/sample-15s.mp3" in
+  
+  let _ = Element.set_state pipeline State_playing in
+
+  let bus = Bus.of_element pipeline in
+
+  match Bus.timed_pop_filtered bus [Bus.(`End_of_stream); Bus.(`Error)] with
+  |{payload=Bus.(`Error s); _} -> raise (Error s)
+  |_ -> ()
+
+
+
+
+
+
+
+
+  
+>>>>>>> Stashed changes
--- camlify/src/streamer.mli
+++ camlify/src/streamer.mli
@@ -1,2 +1,16 @@
 (* TODO: Populate. This file take in a file using gstreamer and
  * makes audio play on a computer *)
+<<<<<<< Updated upstream
+=======
+
+ type file
+
+ type streamer
+ 
+ val play : unit -> unit
+
+
+ (*[parse song] Takes in an mp3 file and parses data.
+ Outputs this data in standardized format*)
+ val parse  : 'a -> 'b
+>>>>>>> Stashed changes
