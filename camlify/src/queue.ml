e83d739d91cee0d7e2cdb0033ded5dbc1b18cc83
Sarah Ha <nh285@cornell.edu>
2022-03-09 17:32:05 -0500

    First implementation of queue

--- camlify/src/queue.ml
+++ camlify/src/queue.ml
@@ -1,1 +1,74 @@
-(* TODO: Implement based on queue.mli *)
+type t = {
+  current_song_id : string;
+  current_song_idx : int;
+  played_songs : string list;
+  following_songs: string list;
+  playlist : string list;
+}
+
+let init_state music_data=
+  {
+    current_song_id = MusicData.initial_song music_data;
+    current_song_idx = 0;
+    played_songs =  [];
+    following_songs = MusicData.playlist;
+    playlist = MusicData.playlist;
+  }
+
+let current_song_id st = st.current_song_id
+let played_songs st = st.played_songs
+
+type result = 
+|Legal of t
+|Illegal
+
+let remove_dup lst = List.sort_uniq compare lst
+
+
+let play_song_by_name song_id music_data st =  
+  try
+    List.find (fun x -> x = song_id) (MusicData.songs music_data st.current_song_id)
+    |> fun x ->
+      Legal{
+        current_song_id = song_id;
+        current_song_idx = MusicData.song_idx music_data current_song_id x;
+        played_songs = List.cons
+        (MusicData.next_song music_data current_song_id x)
+        (played_songs st) |> remove_dup;
+        following_songs = st.following_songs;
+        playlist = st.playlist;
+      }
+    with Not_found -> Illegal
+
+let play_song_by_idx idx music_data st = 
+  try 
+  List.nth (MusicData.songs music_data) idx |> fun x ->
+    Legal{
+      current_song_id = x;
+      current_song_idx = idx;
+      played_songs = List.cons
+      (MusicData.next_song music_data current_song_id x)
+      (played_songs st) |> remove_dup;
+      following_songs = st.following_songs;
+      playlist = st.playlist;
+    }
+  with Failure x -> Illegal
+
+let add_song_to_playlist song music_data st = 
+    try 
+    List.find (fun x -> x = song.song_id) (MusicData.songs music_data st.current_song_id)
+    |> fun x -> Legal st
+with Not_found -> Legal {
+    current_song_id = st.current_song_id;
+    current_song_idx = st.current_song_idx;
+    played_songs = st.played_songs;
+    following_songs = st.following_songs :: [song.song_id];
+    playlist = st.playlist :: [song.song_id];
+
+}
+
+let remove_song_from_playlist song 
+
+let next_song music_data st =  play_song_by_idx (st.current_song_idx+1) music_data st
+
+let back music_data st = play_song_by_idx (st.current_song_idx-1) music_data st
--- camlify/src/queue.mli
+++ camlify/src/queue.mli
@@ -1,2 +1,38 @@
 (* TODO: Populate. This file is the "state" of the player.
  * Current song being played, list of previous songs, next songs, etc. *)
+
+
+type t 
+(** The abstract type of values representing current state of music player*)
+
+val init_state : t
+
+
+val current_song_id : t -> string
+(** [current_song st] is the identifier of the song the user is currently playing*)
+
+val following_songs : t -> string list
+(** [following_songs st] is the list of songs that are left to play on the playlist*)
+
+val played_songs : t-> string list
+(** [played_songs st] is the list of songs that have previously been played on the playlist*)
+
+val remove_dup : 'a list -> 'a list
+(**[remove_dup lst] is a helper function that removes duplicates in a lst*)
+
+type result = 
+| Legal of t
+|Illegal of t
+
+val play_song_by_name : string -> t 
+(** [play_song_in_playlist song_name] is *)
+
+val play_song_by_idx : int -> t
+
+val add_song_to_playlist : string -> t
+
+val remove_song_from_playlist : string -> t
+
+val next_song : t 
+
+val back : t
