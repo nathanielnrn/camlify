e96be2bdf09c44019aab36ef7fc4e8f119aaba88
Nathaniel Navarro <nrn25@cornell.edu>
2022-03-09 17:19:18 -0500

    more build changes

--- camlify/bin/main.ml
+++ camlify/bin/main.ml
@@ -1,3 +1,3 @@
 (* TODO: update with interface to client using terminal,
  * see a2 bin/main.ml for direction *)
-let () = print_endline "Hello, World!"
+let () = Camlify.Streamer.play()
--- camlify/src/streamer.mli
+++
@@ -1,2 +0,0 @@
-(* TODO: Populate. This file take in a file using gstreamer and
- * makes audio play on a computer *)
