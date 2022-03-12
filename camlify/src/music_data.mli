6327fdee3f5745c00450435975d3afd00c242364
Diego <hdk35@cornell.edu>
2022-03-12 15:35:59 -0500

    Testing commit

--- camlify/src/music_data.mli
+++ camlify/src/music_data.mli
@@ -1,2 +1,18 @@
 (* TODO: Populate. This module reads and writes to files in data/ directory.
  * These files contain info on playlists, settings, etc. *)
+
+
+type song
+(**represents a song*)
+
+
+type playlist
+(**represents a playlist*)
+
+
+type interface
+(**represents all the song and playlist data available*)
+
+val from_json : Yojson.Basic.t -> t
+(** [from_json j] is the data that [j] represents. Requires: [j] is
+    a valid JSON music data representation. *)
