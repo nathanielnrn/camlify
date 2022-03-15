3f0faae2115873ed49b4bd154796481f633bc8ad
Diego <hdk35@cornell.edu>
2022-03-15 18:31:43 -0400

    adds the functions to the mli files that queue needs

--- camlify/src/music_data.mli
+++ camlify/src/music_data.mli
@@ -17,4 +17,12 @@ val from_json : Yojson.Basic.t -> interface
 (** [from_json j] is the data that [j] represents. Requires: [j] is
     a valid JSON music data representation.*)
 
-val test_write : unit
+val select_playlist : string -> string list
+(**[select_playlist playlist_name] returns the list of song names that the 
+playlist of playlist_name contains*)
+   
+val list_of_playlist : string list
+(**[list_of_playlist] is a list of all playlist names*)
+   
+val all_songs : string list
+(**[all_songs] is a list of all song names*)
