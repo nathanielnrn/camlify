293925e5b68d0b2cccc475e8c91dc17bbe64eab8
Sarah Ha <nh285@cornell.edu>
2022-03-15 01:10:35 -0400

    values needed in queue.ml

--- camlify/src/music_data.mli
+++ camlify/src/music_data.mli
@@ -1,2 +1,11 @@
-(* TODO: Populate. This module reads and writes to files in data/ directory.
- * These files contain info on playlists, settings, etc. *)
+(* These are values queue.ml needs from music_data.ml copy and paste these specifications *)
+
+ val select_playlist : string -> string list
+ (**[select_playlist playlist_name] returns the list of song names that the 
+ playlist of playlist_name contains*)
+
+ val list_of_playlist : string list
+ (**[list_of_playlist] is a list of all playlist names*)
+
+ val all_songs : string list
+ (**[all_songs] is a list of all song names*)
