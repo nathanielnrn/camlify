db773f47460f6c6187017c123aa8c27468a7ef59
Nathaniel Navarro <nrn25@cornell.edu>
2022-03-15 21:55:28 -0400

    Merging music_data with main? might be final time for MS1 (probably not)

--- camlify/src/music_data.mli
+++ camlify/src/music_data.mli
@@ -1,11 +1,15 @@
-(* These are values queue.ml needs from music_data.ml copy and paste these specifications *)
+(* TODO: Populate. This module reads and writes to files in data/ directory.
+ * These files contain info on playlists, settings, etc. *)
 
- val select_playlist : string -> string list
- (**[select_playlist playlist_name] returns the list of song names that the 
- playlist of playlist_name contains*)
 
- val list_of_playlist : string list
- (**[list_of_playlist] is a list of all playlist names*)
-
- val all_songs : string list
- (**[all_songs] is a list of all song names*)
+val select_playlist : string -> string list
+(**[select_playlist playlist_name] returns the list of song names that the 
+playlist of playlist_name contains 
+For now, the function requiers an existing playlist and deals with a non
+existing playlist by returning the empty list*)
+   
+val list_of_playlist : string list
+(**[list_of_playlist] is a list of all playlist names*)
+   
+val all_songs : string list
+(**[all_songs] is a list of all song names*)
