fd6feafb63ea408de955ecece2f32bad2d950255
Sarah Ha <nh285@cornell.edu>
2022-04-20 17:39:43 -0400

    changed name of some commands

--- camlify/src/command.ml
+++ camlify/src/command.ml
@@ -118,15 +118,15 @@ let parse (str : string) : command =
           else RemoveSong (String.concat " " tl)
         else if String.equal hd "pls" then
           if List.length tl != 0 then raise Malformed else ViewPlaylists
-        else if String.equal hd "pl_ar" then
+        else if String.equal hd "play_artist" then
           if List.length tl != 0 then raise Malformed else PlayArtist
-        else if String.equal hd "pl_al" then
+        else if String.equal hd "play_album" then
           if List.length tl != 0 then raise Malformed else PlayAlbum
-        else if String.equal hd "pl_y" then
+        else if String.equal hd "play_year" then
           if List.length tl != 0 then raise Malformed else PlayYear
-        else if String.equal hd "pl_l" then
+        else if String.equal hd "play_liked" then
           if List.length tl != 0 then raise Malformed else PlayLiked
-        else if String.equal hd "pl_t" then
+        else if String.equal hd "play_tag" then
           if List.length tl != 0 then raise Malformed else PlayTag
         else if String.equal hd "help" then
           if List.length tl != 0 then raise Malformed else Help
